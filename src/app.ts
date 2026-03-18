c51b58b941ff6b46c52cd000335d28aded6c8c06
Snu Seam <siamahme766@gmail.com>
2026-03-19 02:37:26 +0600

    user

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express from "express"
 import { postRoutes } from "./module/post/post.route";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors"
 
 const app = express();
+
+app.use(cors({
+    origin : process.env.APP_URL || "http://localhost:4000",
+    credentials : true 
+}))
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
 app.use(express.json())
 app.get('/',(req,res)=>{
     console.log("Next level web developer");
